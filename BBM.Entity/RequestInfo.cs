e98b1546590d9c131a8073aab402ae25fef5f104
Protick Roy <31928806+protickr@users.noreply.github.com>
2018-06-07 13:40:09 +0600

    Add files via upload

---
+++ BBM.Entity/RequestInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BBM.Entity
+{
+    public class RequestInfo
+    {
+        public int UserID { get; set; }
+        public string UserName { get; set; }
+        public string RequestTypeID { get; set; }
+        public string BloodID { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ BBM.Entity/UserInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BBM.Entity
+{
+    public class UserInfo
+    {
+        public int UId { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Age { get; set; }
+        public int AddressId { get; set; }
+        public int BloodId { get; set; }
+        public int UserRole { get; set; }
+        public string Password { get; set; }
+        public System.DateTime Date { get; set; }
+        public string BloodGroup { get; set; } //specially for edit details
+    }
+}
